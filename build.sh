a9bcff04d24af90f9d06bb48854dc994ab2643ef
hafeezabdul8 <hafidhiabdul2004@gmail.com>
2025-12-31 15:30:48 -0500

    Fix build.sh for Render deployment

--- build.sh
+++ build.sh
@@ -1,15 +1,11 @@
-nano build.sh#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -o errexit
 
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 
 echo "Running migrations..."
-python manage.py migrate#!/usr/bin/env bash
-set -o errexit
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
 python manage.py migrate
