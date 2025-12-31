c4279c71091082cdea9b4b18013b84d1adb4dbfe
hafeezabdul8 <hafidhiabdul2004@gmail.com>
2025-12-31 10:57:49 -0500

    Add build.sh and gunicorn for Render deployment

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
-#!/usr/bin/env bash
+nano build.sh#!/bin/bash
+set -e
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Running migrations..."
+python manage.py migrate#!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
